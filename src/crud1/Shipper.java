55f41a64e362ff6c6caa3139dcc3055739e23dda
Yusuf Eka <yusufblegoh@gmail.com>
2016-03-26 16:00:37 +0700

    Menambah hak akes untuk create. Menambah fitur edit dan delete

--- src/crud1/Shipper.java
+++ src/crud1/Shipper.java
@@ -7,6 +7,7 @@ package crud1;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -17,11 +18,11 @@ public class Shipper extends javax.swing.JFrame {
 
     private Koneksi kon;
     private Object[] header = {"No", "Nama Product", "Harga"};
-    DefaultTableModel tableModel = new DefaultTableModel(null,header);
-    
+    DefaultTableModel tableModel = new DefaultTableModel(null, header);
+
     /**
-     * 
-     * @throws SQLException 
+     *
+     * @throws SQLException
      */
     public Shipper() throws SQLException {
         kon = new Koneksi("root", "hornline", "northwind");
@@ -29,34 +30,38 @@ public class Shipper extends javax.swing.JFrame {
         tabel.setModel(tableModel);
         showData();
     }
-    
+
     /**
-     * Melakukan pengambilan data dari query
-     * Menghapus semua row pada tabel model
-     * di ganti dengan row yang baru
-     * @throws SQLException 
+     * Melakukan pengambilan data dari query Menghapus semua row pada tabel
+     * model di ganti dengan row yang baru
+     *
+     * @throws SQLException
      */
-    private void showData() throws SQLException{
+    private void showData() throws SQLException {
         String sql = "select * from Shippers";
-        for (int i = tableModel.getRowCount()-1; i > 0; i--) {
+        for (int i = tableModel.getRowCount() - 1; i >= 0; i--) {
             tableModel.removeRow(i);
         }
         ResultSet rs = kon.getResult(sql);
-        while (rs.next()) {            
+        while (rs.next()) {
             String kolom[] = new String[3];
             for (int i = 0; i < kolom.length; i++) {
-                kolom[i] = rs.getString(i+1);
+                kolom[i] = rs.getString(i + 1);
             }
             tableModel.addRow(kolom);
         }
     }
-    
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
-        tabel = new javax.swing.JTable();
+        tabel = new javax.swing.JTable(){
+            public boolean isCellEditable(int row, int col){
+                return false;
+            }
+        };
         btnAdd = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
@@ -64,6 +69,13 @@ public class Shipper extends javax.swing.JFrame {
         phone = new javax.swing.JTextField();
         btnSave = new javax.swing.JButton();
         btnCancel = new javax.swing.JButton();
+        btnEdit = new javax.swing.JButton();
+        btnDelete = new javax.swing.JButton();
+        lblId = new javax.swing.JLabel();
+        valueId = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -78,6 +90,11 @@ public class Shipper extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
+        tabel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tabelMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(tabel);
 
         btnAdd.setText("Add New Shipper");
@@ -111,6 +128,30 @@ public class Shipper extends javax.swing.JFrame {
             }
         });
 
+        btnEdit.setText("Edit");
+        btnEdit.setEnabled(false);
+        btnEdit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEditActionPerformed(evt);
+            }
+        });
+
+        btnDelete.setText("Delete");
+        btnDelete.setEnabled(false);
+        btnDelete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDeleteActionPerformed(evt);
+            }
+        });
+
+        jMenu1.setText("Shippper");
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Product");
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -118,88 +159,177 @@ public class Shipper extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnAdd)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel2))
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(btnSave, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                .addComponent(btnAdd)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(btnCancel, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(companyName)
-                            .addComponent(phone))))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addComponent(btnEdit, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(btnDelete, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel1)
+                                    .addComponent(jLabel2)
+                                    .addComponent(lblId))
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(valueId)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addGroup(layout.createSequentialGroup()
+                                            .addComponent(btnSave, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                            .addComponent(btnCancel, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(companyName)
+                                        .addComponent(phone)))))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(btnAdd)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnAdd)
+                    .addComponent(btnEdit)
+                    .addComponent(btnDelete))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 9, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblId)
+                    .addComponent(valueId))
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(companyName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(phone))
+                    .addComponent(phone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnSave)
                     .addComponent(btnCancel))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 8, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addGap(34, 34, 34)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(34, 34, 34))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddActionPerformed
+        if (Login.title.equals("Vice President, Sales")) {
+           tampilkanForm(); 
+        }else{
+            JOptionPane.showMessageDialog(this, "anda tidak berhak");
+        }
+        
+    }//GEN-LAST:event_btnAddActionPerformed
+
+    private void tampilkanForm() {
         btnAdd.setEnabled(false);
         companyName.setEnabled(true);
         phone.setEnabled(true);
         btnCancel.setEnabled(true);
         btnSave.setEnabled(true);
-    }//GEN-LAST:event_btnAddActionPerformed
+    }
 
     private void btnCancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelActionPerformed
-        companyName.setText("");
-        phone.setText("");
-        btnSave.setEnabled(false);
-        btnCancel.setEnabled(false);
-        btnAdd.setEnabled(true);
+        selesaiSave();
     }//GEN-LAST:event_btnCancelActionPerformed
 
     /**
      * Melakukan insert ke table Shippers
-     * @param evt 
+     *
+     * @param evt
      */
     private void btnSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSaveActionPerformed
         try {
             String companyName = this.companyName.getText();
             String phone = this.phone.getText();
-            String query = "insert into Shippers values(NULL,'"+companyName+"','"+phone+"')";
+            String query;
+            if (valueId.getText().equals("")) {
+                query = "insert into Shippers values(NULL,'" + companyName + "','" + phone + "')";
+            } else {
+                String id = valueId.getText();
+                query = "update Shippers set CompanyName = '" + companyName + "', Phone = '" + phone + "' where ShipperID = " + id;
+            }
             kon.execute(query);
             showData();
+            selesaiSave();
         } catch (Exception ex) {
             System.out.println(ex);
         }
     }//GEN-LAST:event_btnSaveActionPerformed
 
+    private void selesaiSave() {
+        lblId.setText("");
+        valueId.setText("");
+        companyName.setText("");
+        phone.setText("");
+        companyName.setEnabled(false);
+        phone.setEnabled(false);
+        btnAdd.setEnabled(true);
+        btnSave.setEnabled(false);
+        btnCancel.setEnabled(false);
+        tabel.setEnabled(true);
+    }
+
+    private void tabelMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tabelMouseClicked
+        btnEdit.setEnabled(true);
+        btnDelete.setEnabled(true);
+    }//GEN-LAST:event_tabelMouseClicked
+
+    private void btnEditActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditActionPerformed
+        tabel.setEnabled(false);
+        btnEdit.setEnabled(false);
+        btnDelete.setEnabled(false);
+        int baris = tabel.getSelectedRow();
+        String id = tabel.getValueAt(baris, 0).toString();
+        String name = tabel.getValueAt(baris, 1).toString();
+        String phone = tabel.getValueAt(baris, 2).toString();
+        tampilkanForm();
+        lblId.setText("Shipper ID");
+        valueId.setText(id);
+        companyName.setText(name);
+        this.phone.setText(phone);
+    }//GEN-LAST:event_btnEditActionPerformed
+
+    private void btnDeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDeleteActionPerformed
+        int confirm = JOptionPane.showConfirmDialog(this, "Apakah anda yakin ?");
+        if (confirm == JOptionPane.YES_OPTION) {
+            int baris = tabel.getSelectedRow();
+            String sql = "delete from Shippers where ShipperID = " + tabel.getValueAt(baris, 0).toString();
+            try {
+                kon.execute(sql);
+                showData();
+                selesaiSave();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+
+        }
+    }//GEN-LAST:event_btnDeleteActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAdd;
     private javax.swing.JButton btnCancel;
+    private javax.swing.JButton btnDelete;
+    private javax.swing.JButton btnEdit;
     private javax.swing.JButton btnSave;
     private javax.swing.JTextField companyName;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lblId;
     private javax.swing.JTextField phone;
     private javax.swing.JTable tabel;
+    private javax.swing.JLabel valueId;
     // End of variables declaration//GEN-END:variables
 }
