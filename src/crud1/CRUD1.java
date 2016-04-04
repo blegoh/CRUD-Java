ff2b6e60c0998a177702e3b7698f19a4d9803068
Yusuf Eka <yusufblegoh@gmail.com>
2016-04-05 06:28:19 +0700

    Add look and feel

--- src/crud1/CRUD1.java
+++ src/crud1/CRUD1.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package crud1;
 
 import java.sql.SQLException;
@@ -17,6 +13,16 @@ public class CRUD1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws SQLException {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        }catch(Exception e){
+            
+        }
         new Login().setVisible(true);
     }
     
